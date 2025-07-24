5ff549d7f7c491c92b4046cd500a341acbc8b9d4
dfradehubs <dfradejas@freepik.com>
2025-07-24 20:03:24 +0200

    First commit helm registry branch

--- api/v1alpha1/common_types.go
+++
@@ -1,9 +0,0 @@
-package v1alpha1
-
-// SecretRef TODO
-type SecretRef struct {
-	Name        string `json:"name"`
-	Namespace   string `json:"namespace,omitempty"`
-	KeyUsername string `json:"keyUsername"`
-	KeyPassword string `json:"keyPassword"`
-}
